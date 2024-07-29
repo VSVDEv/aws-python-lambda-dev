af3a904f2fc0524a8381d664612f34d82554fd6c
VSVDev <vsvdev@example.com>
2024-07-29 17:08:26 +0000

    first

--- dictate-function/app.py
+++ dictate-function/app.py
@@ -10,8 +10,7 @@ from contextlib import closing
 dynamoDBResource = boto3.resource('dynamodb')
 pollyClient = boto3.client('polly')
 s3Client = boto3.client('s3', endpoint_url='https://s3.' +
-                        os.environ['AWS_REGION'] + '.amazonaws.com')
-
+             os.environ['AWS_REGION'] + '.amazonaws.com')
 
 def lambda_handler(event, context):
 
@@ -48,7 +47,7 @@ def getNote(dynamoDBResource, ddbTable, UserId, NoteId):
         }
     )
 # TODO 1: Get the note text from the pollynotes DynamoDB table that matches the UserId and NoteId
-
+    return records['Item']['Note']
     # End TODO 1
 
 
@@ -56,8 +55,12 @@ def createMP3File(pollyClient, text, VoiceId, NoteId):
     print("createMP3File Function")
     # TODO 2: Use polly to convert the note text to speech using the VoiceId
     # and save the file as an MP3 in the /tmp folder
-
-    # End TODO 2
+    pollyResponse = pollyClient.synthesize_speech(
+        OutputFormat='mp3',
+        Text=text,
+        VoiceId=VoiceId
+    )
+ # End TODO 2
     if "AudioStream" in pollyResponse:
         postId = str(NoteId)
         with closing(pollyResponse["AudioStream"]) as stream:
@@ -71,7 +74,9 @@ def createMP3File(pollyClient, text, VoiceId, NoteId):
 def hostFileOnS3(s3Client, filePath, mp3Bucket, UserId, NoteId):
     print("hostFileOnS3 Function")
     # TODO 3: Upload the mp3 file to S3 mp3Bucket and generate a pre-signed URL to access the MP3 object
-
+    s3Client.upload_file(filePath,
+                         mp3Bucket,
+                         UserId+'/'+NoteId+'.mp3')
     # End TODO 3
 
     # Remove the file from the temp location to avoid potential data leaks
